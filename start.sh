49c1fec4d27116d72a6e298ef2ef41a380493922
Javier Roman <javieroman@gmail.com>
2026-04-06 19:01:36 +0200

    fix: use node:20-alpine as base so better-sqlite3 native module works

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-DATA_DIR=/data node /api/index.js &
-nginx -g 'daemon off;'
+# Start API
+export DATA_DIR=/data
+node /api/index.js &
+
+# Wait for API to be ready before nginx starts
+sleep 2
+
+# Start nginx in foreground (keeps container alive)
+exec nginx -g 'daemon off;'
